a11d863428cb8e69b8833d0868dbd3df4fc82d0f
Besmelh Alshaalan <besmelh.s@gmail.com>
2025-03-27 05:38:12 +0300

    removed unused vars

--- project-gutenberg/src/app/page.tsx
+++ project-gutenberg/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BookFetcher from "../components/BookFetcher";
 import BookHistory from "../components/BookHistory";
 import BookDisplay from "../components/BookDisplay";
